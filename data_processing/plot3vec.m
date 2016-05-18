a6174a36298fe7461488426534783b9ec80aebd6
Hyunsoo Kim <hk94@duke.edu>
2016-05-18 16:47:40 -0400

    handle different number of input arguments

--- data_processing/plot3vec.m
+++ data_processing/plot3vec.m
@@ -1,6 +1,11 @@
-function plot3vec(v, length) % assumes input is N x 3 matrix
+function plot3vec(v, length, style) % assumes input is N x 3 matrix
 
+% if length of vector is not given, set it to 1
 if nargin == 1
+    length = ones(size(v,1),1);
+    style = '-k.';
+elseif nargin == 2
+    style = length;
     length = ones(size(v,1),1);
 end
 
@@ -8,7 +13,7 @@ v = v.*repmat(length,1,3); % replicate theta vector into Nx3 matrix, and multipl
 r = reshape(v,1,[]); % put data all in one row
 r = [zeros(size(r));r]; % interleave 0's in between the data
 r = reshape(r,[],3); % reshape back to 2N x 3
-plot3(r(:,1),r(:,2),r(:,3),'-k.'); % plot it
+plot3(r(:,1),r(:,2),r(:,3),style); % plot it
 grid on
 
 end
